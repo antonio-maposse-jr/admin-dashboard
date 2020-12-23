34fb84c04251afaad7f3ebf4703da0dd44b4129d
Antonio <antonio.maposse.jr@gmail.com>
2020-12-23 14:43:21 +0200

    Client API Duplication of Clients Fixed

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -13,15 +13,21 @@ class ClientsController extends Controller
     public function store(Request $request)
     {
         $client = new Clients();
-        $client->name = $request->input_name;
-        $client->surname = $request->input_surname;
-        $client->date_birth = $request->input_dob;
-        $client->phone = $request->input_phone;
-        $client->id_type = $request->input_docType;
-        $client->id_number = $request->input_docNumber;
+        $client_exists = Client::where('phone', $request->input_phone)->exists();
+        if (!$client_exists){
+            $client->name = $request->input_name;
+            $client->surname = $request->input_surname;
+            $client->date_birth = $request->input_dob;
+            $client->phone = $request->input_phone;
+            $client->id_type = $request->input_docType;
+            $client->id_number = $request->input_docNumber;
 
-        $client->save();
-        return response()->json($client, 200);
+            $client->save();
+            return response()->json($client, 200);
+        }else{
+            return response()->json(['message'=>'client already exists'], 409);
+        }
+
 
     }
 
